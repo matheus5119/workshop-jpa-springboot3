e0e8a89aeea518aa5b0c0104e2ac2b2f5a70e037
matheus <matheussantos.548@gmail.com>
2024-07-06 21:25:52 -0300

    Product

---
+++ src/main/java/com/projeto1/demo/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.projeto1.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projeto1.demo.entities.Product;
+import com.projeto1.demo.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+			Optional<Product> obj = repository.findById(id);
+			return obj.get();
+	}
+}
