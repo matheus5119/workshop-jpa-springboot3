e2cc82592276cbc66d4a8913411a66683f1e021d
matheus <matheussantos.548@gmail.com>
2024-07-14 11:32:24 -0300

    Exception handling - delete

--- src/main/java/com/projeto1/demo/services/UserService.java
+++ src/main/java/com/projeto1/demo/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.projeto1.demo.entities.User;
 import com.projeto1.demo.repositories.UserRepository;
+import com.projeto1.demo.services.exceptions.DatabaseException;
 import com.projeto1.demo.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,6 +33,12 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 }
